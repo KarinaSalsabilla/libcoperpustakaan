98e7964fecf105a2157ea7305fb463bd3b205c73
LENOVO <ayixdjoko@gmail.com>
2026-04-14 09:08:47 +0700

    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
+    <meta name="google-site-verification" content="KnkSdrRC2Xpfhw7n2LcjJmTAt0p22ttk9PJw_vpHTyc" />
     <title>Libco-perpustakaan digital</title>
 
     <!-- Fonts -->
